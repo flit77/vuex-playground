cc1ea9918a5507f60b2f328cd34e9f883247a30a
Vasiliy Shubko <flit77@gmail.com>
2017-05-02 01:01:38 +0200

    actions and mutations added

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,30 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
-    <div>
-      Counter: {{ $store.state.count }} times, count is {{ evenOrOdd }}.
+    <div class="container">
+      <div class="row text-center">
+          <h3>Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.</h3>
+          <button class="btn btn-success" @click="increment">+</button>
+          <button class="btn btn-danger" @click="decrement">-</button>
+          <button class="btn" @click="incrementIfOdd">Increment if odd</button>
+          <button class="btn" @click="incrementAsync">Increment async</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 export default {
   name: 'hello',
   computed: mapGetters([
     'evenOrOdd'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement',
+    'incrementIfOdd',
+    'incrementAsync'
   ])
 }
 
